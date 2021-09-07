a0e38f447e695d2d0935a01fa3666cce5e95ae84
Risky Kurniawan <62739625+riskykurniawan15@users.noreply.github.com>
2021-09-07 11:30:51 +0700

    Initial commit

---
+++ Segitiga.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	fmt.Println("================================")
+	fmt.Println("    PROGRAM CETAK SEGITIGA")
+	fmt.Println("================================")
+	var jenis string
+	var jml, index int
+	fmt.Print("Masukan jenis segitiga (terbalik / tidak) ? ")
+	fmt.Scan(&jenis)
+	fmt.Print("Masukan jumlah baris ? ")
+	fmt.Scan(&jml)
+
+	jenis = strings.ToLower(jenis)
+
+	if jenis == "terbalik" {
+		index = (jml*2)-1
+		for i:=jml; i>=1; i-- {
+			for j:=0; j<(jml-i); j++ {
+				fmt.Print(" ")
+			}
+			for k:=1; k<=index; k++ {
+				fmt.Print("*")
+			}
+			fmt.Println()
+			index-=2
+		}
+	}else if jenis == "tidak"{
+		index = 1
+		for i:=1; i<=jml; i++ {
+			for j:=(jml-i); j>=0; j-- {
+				fmt.Print(" ")	
+			}	
+			for k:=1; k<=index; k++ {
+				fmt.Print("*")	
+			}	
+			fmt.Println()
+			index += 2
+		}
+	}else{
+		fmt.Println("Pilihan tidak tersedia!!")
+	}
+}
